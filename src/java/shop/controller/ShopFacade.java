eadb781b850845bd7a6dcea95a2653142a8bdc18
mgordo <m.gordo8@gmail.com>
2016-01-06 01:52:25 +0100

    More minor additions

--- src/java/shop/controller/ShopFacade.java
+++ src/java/shop/controller/ShopFacade.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shop.controller;
+
+import javax.ejb.Stateless;
 
 /**
  *
  * @author Miguel
  */
+@Stateless
 public class ShopFacade {
-    
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
 }
